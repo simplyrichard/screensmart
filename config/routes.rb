753e059fcac92ddfa74772d1d2d7653cd1c1325a
timraasveld <ttimitri@gmail.com>
2016-03-16 12:02:34 +0100

    Set root URL properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount Rack::HaproxyStatus::Endpoint.new(path: Rails.root.join('config/balancer_state')) => '/load_balancer_status'
-  get '/', to: 'app#index'
+  root to: 'app#index'
 
   resources :responses, defaults: { format: :json }
 end
