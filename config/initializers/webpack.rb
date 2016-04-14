6649f70cfddeb49ad2214ff86332b44d3402d59d
timraasveld <ttimitri@gmail.com>
2016-04-14 14:24:49 +0200

    Fix included JS url

--- config/initializers/webpack.rb
+++ config/initializers/webpack.rb
@@ -5,12 +5,12 @@ if Rails.configuration.webpack[:use_manifest]
   if File.exist?(asset_manifest)
     Rails.configuration.webpack[:asset_manifest] = JSON.parse(
       File.read(asset_manifest)
-    ).with_indifferent_access
+    ).values.first
   end
 
   if File.exist?(common_manifest)
     Rails.configuration.webpack[:common_manifest] = JSON.parse(
       File.read(common_manifest)
-    ).with_indifferent_access
+    ).values.first
   end
 end
