9d4eaf0f1d0956673e3bc7f8b8cd5425a19ee8a3
timraasveld <ttimitri@gmail.com>
2016-06-28 13:35:24 +0200

    Match deploy directory with ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
-server '10.240.90.11', user: 'deploy', roles: %w{ app web }, rails_env: :production
-set :deploy_to, '/var/www/screensmart-playground.roqua.nl'
+ip = '10.240.90.11'
+server ip, user: 'deploy', roles: %w{ app web }, rails_env: :production
+set :deploy_to, "/var/www/#{ip}"
