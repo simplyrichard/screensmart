a46b03fb38245a1377eb9f95aa1bcd2d49bc7a53
timraasveld <ttimitri@gmail.com>
2016-06-28 13:43:06 +0200

    Fix branch name

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,6 @@ lock '3.5.0'
 set :application, 'screensmart'
 set :repo_url, 'https://github.com/roqua/screensmart'
 
-set :branch, :tr-test-default-capistrano-config
+set :branch, :'tr-test-default-capistrano-config'
 
 set :linked_files, fetch(:linked_files, []).push('.env.local')
