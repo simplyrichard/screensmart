28ca6d95ec5cb0de42f647da4f888e3297c51f9f
timraasveld <ttimitri@gmail.com>
2016-07-01 14:27:28 +0200

    Shorten OpenCPU max timeout to 5 seconds because it should never take longer

--- config/initializers/opencpu.rb
+++ config/initializers/opencpu.rb
@@ -3,5 +3,5 @@ OpenCPU.configure do |config|
   config.endpoint_url = ENV['OPENCPU_ENDPOINT_URL']
   config.username     = ENV['OPENCPU_USERNAME']
   config.password     = ENV['OPENCPU_PASSWORD']
-  config.timeout      = ENV['OPENCPU_TIMEOUT'].to_i if ENV['OPENCPU_TIMEOUT']
+  config.timeout      = 5
 end
