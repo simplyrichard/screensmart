d46f70c005a17b8375ad118feedb384ef0394bd3
timraasveld <ttimitri@gmail.com>
2016-07-28 13:50:16 +0200

    Test invitation form
    Merges #24
    
    Squashed commit of the following:
    
    commit aaea6ee813e5cd3748e7db55c4419e3aaa62e952
    Author: timraasveld <ttimitri@gmail.com>
    Date:   Thu Jul 28 13:49:41 2016 +0200
    
        Update invitation form url
    
    commit de08ebefaf6b4b3db95b04800d6dbb3609b74fbb
    Author: timraasveld <ttimitri@gmail.com>
    Date:   Fri Jul 15 12:02:43 2016 +0200
    
        Test invitation form

---
+++ spec/features/sending_invitations_spec.rb
@@ -0,0 +1,38 @@
+describe 'sending invitations' do
+  def fill_out_placeholder(placeholder, value)
+    find("input[placeholder='#{placeholder}']").set value
+  end
+
+  def fill_out_all_fields
+    fill_out_placeholder 'e-mail respondent', 'some@patient.dev'
+    fill_out_placeholder 'uw e-mail', 'some@doctor.dev'
+    choose 'Positieve symptomen voor psychose'
+  end
+
+  before { visit '/' }
+
+  scenario 'filling out all fields and submitting the form' do
+    fill_out_all_fields
+
+    click_on 'Verstuur uitnodiging'
+    expect(page).not_to have_css '.error'
+  end
+
+  scenario 'not filling out all fields' do
+    fill_out_all_fields
+    fill_out_placeholder 'e-mail respondent', ''
+
+    click_on 'Verstuur uitnodiging'
+    expect(page).to have_css '.error'
+    expect(page).to have_content 'Vul een geldig e-mailadres in'
+  end
+
+  scenario 'entering invalid values' do
+    fill_out_all_fields
+    fill_out_placeholder 'e-mail respondent', 'invalid'
+
+    click_on 'Verstuur uitnodiging'
+    expect(page).to have_css '.error'
+    expect(page).to have_content 'Vul een geldig e-mailadres in'
+  end
+end
