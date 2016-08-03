f28d89dbc2e282257cd25b813f028df0130ce39e
Arnold van der Meulen <zwippie@gmail.com>
2016-08-03 12:09:13 +0200

    Do not accept invitation once response has been finished

--- spec/commands/accept_invitation_spec.rb
+++ spec/commands/accept_invitation_spec.rb
@@ -1,13 +1,13 @@
 describe AcceptInvitation do
   subject { described_class.run(params) }
 
-  context 'with valid parameters' do
-    let(:invitation_sent) do
-      SendInvitation.run! respondent_email: 'some@respondent.dev',
-                          requester_email: 'some@doctor.dev',
-                          domain_ids: ['POS-PQ']
-    end
+  let(:invitation_sent) do
+    SendInvitation.run! respondent_email: 'some@respondent.dev',
+                        requester_email: 'some@doctor.dev',
+                        domain_ids: ['POS-PQ']
+  end
 
+  context 'with valid parameters' do
     let(:params) { { invitation_uuid: invitation_sent.invitation_uuid } }
     it 'creates an InvitationAccepted event' do
       expect { subject }.to change { Events::InvitationAccepted.count }.by(1)
@@ -16,6 +16,19 @@ describe AcceptInvitation do
 
   context 'invitation_uuid is missing' do
     let(:params) { { invitation_uuid: '' } }
+    it { is_expected.to have(1).errors_on :invitation_uuid }
+  end
+
+  context 'response has already been finished' do
+    let(:params) { { invitation_uuid: invitation_sent.invitation_uuid } }
+
+    before do
+      Events::ResponseFinished.create!(
+        invitation_uuid: invitation_sent.invitation_uuid,
+        response_uuid: SecureRandom.uuid
+      )
+    end
+
     it { is_expected.to have(1).errors_on :invitation_uuid }
   end
 end
